0fdba1620a3e1e197fc7ac156cbd25ac885117c8
Mandar U Jog <mjog@google.com>
2021-02-24 18:05:49 +0000

    WIP

---
+++ extensions/jwt_header/README.md
@@ -0,0 +1,76 @@
+# WebAssembly
+
+This plugin can be compiled and run via the Envoy WebAssembly support.
+
+## Creating build Docker image.
+
+Follow the instructions in the github.com/istio/envoy/api/wasm/cpp/README.md to build the WebAssembly Docker build image.
+
+## Build via the Docker image.
+
+```bash
+./build_wasm.sh
+```
+
+# jwt_header filter
+
+Jwt header filter reads a jwt token and populates header from claims based on configuration.
+The headers that are populated according to configuration can be used in header based routing.
+This filter is compiled as webassembly and loaded into Envoy.
+
+
+`testdata/istio/jwt_filter.yaml` is the EnvoyFilter specification to add filters in a predefined way.
+`testdata/istio/bookinfo/*` contain Istio configuration that exposes reviews and uses header based routing at ingress.
+
+
+The Jwt header filter is desiged to specifically work with the jwt_authn filter. Jwt_authn filter validates jwt token and if valid
+populates dynamic metadata. Jwt header filter reads the validated token from this place.
+
+## How to deploy
+* Update ingress-gateway deployment image with a build of proxy 1.5 that bundles in the jwt-header wasm module.
+ 
+  `kubectl -n istio-system set image deployment/istio-ingressgateway istio-proxy=gcr.io/mixologist-142215/proxyv2:1.5-jwtHeader`
+
+* Deploy the filter at ingress using canned configuration.
+
+  `kubectl apply -f testdata/istio/jwt_filter.yaml -n istio-system`
+
+The above configuration will deploy jwt validation filter using unsafe keys from testing@secure.istio.io issuer.
+The configuration specifies that a header named `jwt-group` should be populated from the `group` claim in the validated jwt token.
+
+`testdata/*_token.txt` files contain pre-generated tokens with group claim of canary, dev, and prod.
+
+If you have deployed the `bookinfo` application you may try jwt based routing by running
+`kubectl apply -f testdata/istio/bookinfo/` 
+
+
+The virtual service maps the following jwt group claims to reviews versions.
+Different review versions have slightly different json output.
+
+```yaml
+{prod: v1, canary: v2, dev: v3}
+```
+The following is an example testing session. 
+
+```bash
+$ export GW_IP=35.184.6.129
+$ cd extensions/jwt_header/testdata (jwt_header_sample)
+$ curl http://${GW_IP}/reviews/0  -H "Authorization: Bearer $(cat canary_token.txt)" -H "Host: reviews.local"  
+{"id": "0","reviews": [{  "reviewer": "Reviewer1",  "text": "An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!", 
+"rating": {"stars": 5, "color": "black"}},
+{  "reviewer": "Reviewer2",  "text": "Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare.", 
+"rating": {"stars": 4, "color": "black"}}]}
+
+$ curl http://${GW_IP}/reviews/0 -H "Authorization: Bearer $(cat dev_token.txt)" -H "Host: reviews.local"  
+{"id": "0","reviews": [{  "reviewer": "Reviewer1",  "text": "An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!",
+"rating": {"stars": 5, "color": "red"}},{  "reviewer": "Reviewer2",  "text": "Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare.",
+"rating": {"stars": 4, "color": "red"}}]}
+
+$ curl http://${GW_IP}/reviews/0 -H "Authorization: Bearer $(cat prod_token.txt)" -H "Host: reviews.local"  
+{"id": "0","reviews": [{  "reviewer": "Reviewer1",  "text": "An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!"},{  "reviewer": "Reviewer2",  "text": "Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare."}]}
+```
+
+
+## Limitations
+* Only supports accessing top level claims
+* Does not support multi valued claims, like lists.
---
+++ extensions/jwt_header/plugin.cc
@@ -0,0 +1,150 @@
+/* Copyright 2019 Istio Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "extensions/jwt_header/plugin.h"
+
+//#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+
+#include "absl/strings/str_cat.h"
+#include "google/protobuf/util/json_util.h"
+
+#ifndef NULL_PLUGIN
+
+#include "base64.h"
+
+#else  // NULL_PLUGIN
+
+#include "common/common/base64.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Wasm {
+namespace JwtHeader {
+namespace Plugin {
+
+using namespace ::Envoy::Extensions::Common::Wasm::Null::Plugin;
+using NullPluginRegistry =
+    ::Envoy::Extensions::Common::Wasm::Null::NullPluginRegistry;
+
+NULL_PLUGIN_REGISTRY;
+
+#endif  // NULL_PLUGIN
+
+using google::protobuf::util::JsonParseOptions;
+using google::protobuf::util::Status;
+
+static RegisterContextFactory register_JwtHeader(
+    CONTEXT_FACTORY(PluginContext), ROOT_FACTORY(PluginRootContext));
+
+bool PluginRootContext::onConfigure(size_t) {
+  std::unique_ptr<WasmData> configuration = getConfiguration();
+
+  JsonParseOptions json_options;
+  auto status =
+      JsonStringToMessage(configuration->toString(), &config_, json_options);
+  if (status != Status::OK) {
+    LOG_WARN(absl::StrCat("Cannot parse plugin configuration JSON string ",
+                          configuration->toString()));
+    return false;
+  }
+
+  return true;
+}
+
+FilterHeadersStatus PluginContext::onRequestHeaders(uint32_t) {
+  google::protobuf::Struct jwtPayloadStruct;
+  JsonParseOptions json_options;
+
+  // jwt validation filter uses the jwt-auth dynamic metadata
+  // with issuer as the key in struct.
+  //if (!getMessageValue({"metadata", "filter_metadata", "jwt-auth"},
+  if (!getMessageValue({"metadata", "filter_metadata", "envoy.filters.http.jwt_authn"},
+                       &jwtPayloadStruct)) {
+    LOG_DEBUG("No jwt-auth metadata present");
+    return FilterHeadersStatus::Continue;
+  }
+
+  // Istio jwt filter adds exactly one entry to the map
+  // The 'issuer' is not relevant.
+  auto it = jwtPayloadStruct.fields().begin();
+
+  if (jwtPayloadStruct.fields().end() == it) {
+    LOG_DEBUG("Empty jwt metadata");
+    return FilterHeadersStatus::Continue;
+  }
+
+
+  google::protobuf::Struct jwtStruct;
+  std::string jsonJwt;
+
+  google::protobuf::util::MessageToJsonString(it->second.struct_value(), &jsonJwt);
+
+  //auto jwtStruct = it->second.struct_value();
+
+  
+  //auto jsonJwt = it->second.string_value();
+
+  auto status = JsonStringToMessage(jsonJwt, &jwtStruct, json_options);
+
+  if (status != Status::OK) {
+    LOG_WARN(absl::StrCat("Cannot parse JSON string ", jsonJwt));
+    return FilterHeadersStatus::Continue;
+  }
+   
+
+  bool modified_headers = false;
+  const auto end_it = jwtStruct.fields().end();
+
+  for (const auto& mapping : rootContext()->config().header_map()) {
+    const auto claim_it = jwtStruct.fields().find(mapping.second);
+    if (claim_it == end_it) {
+      LOG_DEBUG(
+          absl::StrCat("Claim ", mapping.second, " missing from ", jsonJwt));
+
+      // Remove mapping request header if present so that it is not used to
+      // decide routes.
+      auto found = getRequestHeader(mapping.first);
+      if (found) {
+        removeRequestHeader(mapping.first);
+        modified_headers = true;
+      }
+      continue;
+    }
+
+    if (WasmResult::Ok !=
+        replaceRequestHeader(mapping.first, claim_it->second.string_value())) {
+      LOG_WARN(absl::StrCat("Unable to set header ", mapping.first, " to ",
+                            claim_it->second.string_value()));
+    } else {
+      LOG_DEBUG(absl::StrCat("SetHeader ", mapping.first, " = ",
+                             claim_it->second.string_value()));
+      modified_headers = true;
+    }
+  }
+
+  if (modified_headers) {
+    clearRouteCache();
+  }
+
+  return FilterHeadersStatus::Continue;
+}
+
+#ifdef NULL_PLUGIN
+}  // namespace Plugin
+}  // namespace JwtHeader
+}  // namespace Wasm
+}  // namespace Extensions
+}  // namespace Envoy
+#endif
